c20f7bac0c0f82e3778769c186267bea81a1df66
Facundo Acevedo <facevedo@disroot.org>
2017-09-15 22:23:00 -0300

    modifciones para compatibilizar con los nuevos nodos
    En el nodo mejoramos el constructor

--- src/AStar.java
+++ src/AStar.java
@@ -1,57 +1,61 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import java.util.Collections;
+import ia.battle.core.BattleField;
+import ia.battle.core.ConfigurationManager;
+import ia.battle.core.FieldCell;
+import ia.battle.core.FieldCellType;
 
 public class AStar {
 
-    // Array de mapa
-	private int[][] map;
+
 	//Arra de nodos
-	private List<FieldCell> nodes;
+	private ArrayList<Node> nodes;
 	// Array de nodos [ abiertos y cerrados ]
-	private ArrayList<FieldCell> closedNodes, openNodes;
+	private ArrayList<Node> closedNodes, openNodes;
 	
-	private FieldCell origin, destination;
+	private Node origin, destination;
 
-	public AStar(int[][] map) {
-		// Matriz del mapa (enteros)
-		// abiertos y cerrados?
-		// setter
-		
-		this.map = map;
-	}
 
-	public ArrayList<FieldCell> findPath(int x1, int y1, int x2, int y2) {
+	public ArrayList<Node> findPath(FieldCell origen, FieldCell destino) {
 		// Enuentra la ruta entre dos puntos
 		// Devuelve La lista de nodos por la que pasa
 		
 		// defino un array de nodos ( Tipo nodo)
-		nodes = new ArrayList<FieldCell>();
-		closedNodes = new ArrayList<FieldCell>();
-		openNodes = new ArrayList<FieldCell>();
+		nodes = new ArrayList<Node>();
+		closedNodes = new ArrayList<Node>();
+		openNodes = new ArrayList<Node>();
 
+		//Obtengo todos los field Cells segun el alto y ancho del mapa
+		// alias de x
+		int ancho = ConfigurationManager.getInstance().getMapWidth();
+		// alias de Y
+		int alto = ConfigurationManager.getInstance().getMapHeight();
+		
+		//
 		// A node is added for each passable cell in the map
 		// Filtro los puntos del mapa caminables y los recreo en forma de nodo (grafo)
-		for (int i = 0; i < map.length; i++)
-			for (int j = 0; j < map[i].length; j++) {
-				// se va moviendo por la primer columna uando i=0 y asi sucesivamente 
-				if (map[i][j] == 0)
+		for (int i = 0; i < ancho; i++) // me muevo por el eje X
+			for (int j = 0; j < alto; j++) {  // me muevo por el eje Y
+				//Obtengo el field cell que estoy recorriendo
+				FieldCell fieldCellActual = BattleField.getInstance().getFieldCell(i, j);
+				if (fieldCellActual.getFieldCellType() == FieldCellType.NORMAL )
 					// si el nodo esta caminable/sin pared (?) creo el 
 					// nodo para procesarlo luego
-					nodes.add(new FieldCell(i, j));
+					nodes.add(new Node(fieldCellActual));
 			}
 			
-		// obtengo el nodo (previamente creado ↑ ) a partir de la coordenada pasa 
-		// en la firma de la funcion, osea el punto de partida del camino
-		origin = nodes.get(nodes.indexOf(new FieldCell(x1, y1)));
+		//obtengo de la lista recien creada, el nodo de origen
+		origin = nodes.get(nodes.indexOf(new Node(origen)));
 		
 		
 		//lo mismo pero el destino
-		destination = nodes.get(nodes.indexOf(new FieldCell(x2, y2)));
+		destination = nodes.get(nodes.indexOf(new Node(destino)));
 
-		FieldCell currentNode = origin;
+		//vamoas a iterar sobre currentNode
+		Node currentNode = origin;
 		while (!currentNode.equals(destination)) {
 			processNode(currentNode);
 			currentNode = getMinFValueNode();
@@ -60,9 +64,9 @@ public class AStar {
 		return retrievePath();
 	}
 
-	private ArrayList<FieldCell> retrievePath() {
-		ArrayList<FieldCell> path = new ArrayList<FieldCell>();
-		FieldCell node = destination;
+	private ArrayList<Node> retrievePath() {
+		ArrayList<Node> path = new ArrayList<Node>();
+		Node node = destination;
 
 		while (!node.equals(origin)) {
 			path.add(node);
@@ -74,18 +78,18 @@ public class AStar {
 		return path;
 	}
 
-	private void processNode(FieldCell node) {
+	private void processNode(Node node) {
 		// Obtengo los nodos adyacentes al nodo recibido
 
 		// obtengo los adyacentes
-		ArrayList<FieldCell> adj = getAdjacentNodes(node);
+		ArrayList<Node> adj = getAdjacentNodes(node);
 
 		//saco de la lista de nodos abiertos el nodo en el que estoy parado
 		openNodes.remove(node);
-		// y lo agrego aca
+		// y loa grego aca
 		closedNodes.add(node);
 
-		for (FieldCell n : adj) {
+		for (Node n : adj) {
 			// por cada nodo adyacente  -> n
 
 			if (closedNodes.contains(n))
@@ -98,8 +102,10 @@ public class AStar {
 			h += Math.abs(destination.getY() - n.getY());
 
 			//Compute the distance from origin to node 'n' 
-			int g = node.getG();
+			float g = node.getG();
 			if (node.getX() == n.getX() || node.getY() == n.getY())
+				//Calculamos el costo de moverse en vertical/horizontal o en diagonal
+				//TODO: de donde sacamos estos datos
 				g += 10;
 			else
 				g += 14;
@@ -123,91 +129,53 @@ public class AStar {
 		}
 	}
 
-	private FieldCell getMinFValueNode() {
-		FieldCell node = openNodes.get(0);
+	private Node getMinFValueNode() {
+		Node node = openNodes.get(0);
 
-		for (FieldCell n : openNodes)
+		for (Node n : openNodes)
 			if (node.getF() > n.getF())
 				node = n;
 
 		return node;
 	}
 
-	private ArrayList<FieldCell> getAdjacentNodes(FieldCell node) {
-		ArrayList<FieldCell> adjCells = new ArrayList<FieldCell>();
 
-		int x = node.getX();
-		int y = node.getY();
+	private ArrayList<Node> getAdjacentNodes(Node node) {
+		ArrayList<Node> adjNodes = new ArrayList<Node>();
 
-		if (nodes.indexOf(new FieldCell(x + 1, y)) >= 0)
-			adjCells.add(nodes.get(nodes.indexOf(new FieldCell(x + 1, y))));
 
-		if (nodes.indexOf(new FieldCell(x, y + 1)) >= 0)
-			adjCells.add(nodes.get(nodes.indexOf(new FieldCell(x, y + 1))));
-
-		if (nodes.indexOf(new FieldCell(x - 1, y)) >= 0)
-			adjCells.add(nodes.get(nodes.indexOf(new FieldCell(x - 1, y))));
-
-		if (nodes.indexOf(new FieldCell(x, y - 1)) >= 0)
-			adjCells.add(nodes.get(nodes.indexOf(new FieldCell(x, y - 1))));
-
-		if (nodes.indexOf(new FieldCell(x - 1, y - 1)) >= 0)
-			adjCells.add(nodes.get(nodes.indexOf(new FieldCell(x - 1, y - 1))));
-
-		if (nodes.indexOf(new FieldCell(x + 1, y + 1)) >= 0)
-			adjCells.add(nodes.get(nodes.indexOf(new FieldCell(x + 1, y + 1))));
-
-		if (nodes.indexOf(new FieldCell(x - 1, y + 1)) >= 0)
-			adjCells.add(nodes.get(nodes.indexOf(new FieldCell(x - 1, y + 1))));
-
-		if (nodes.indexOf(new FieldCell(x + 1, y - 1)) >= 0)
-			adjCells.add(nodes.get(nodes.indexOf(new FieldCell(x + 1, y - 1))));
-
-		return adjCells;
-	}
-
-	private void mergePath(ArrayList<FieldCell> path) {
-		for(FieldCell node : path)
-			map[node.getX()][node.getY()] = 2;
+		List<FieldCell> adjFieldCells = BattleField.getInstance().getAdjacentCells(node.getFieldCell());
 		
-	}
-	
-	public void printMap() {
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++)
-				switch (map[i][j]) {
-				case 0:
-					System.out.print("   ");
-					break;
-					
-				case 1:
-					System.out.print("XXX");
-					break;
-					
-				case 2:
-					System.out.print(" o ");
-					break;
-				}
-				
-			System.out.println();
+		for (FieldCell n: adjFieldCells) {
+			// recorro la lista de field cells adjacentes y las "convierto" a nodos y los
+			// agrego a la lista
+			adjNodes.add(new Node(n));
+			
 		}
+
+		return adjNodes;
 	}
 
-//	public static void main(String[] args) {
-//		
-//		MazeGenerator mg = new MazeGenerator((40 - 1) / 4, (40 - 1) / 2);
-//		
-//		AStar a = new AStar(mg.getMaze());
-//		
-//		System.out.println("The maze to resolve:");
-//		a.printMap();
-//
-//		ArrayList<Node> bestPath = a.findPath(1, 1, 35, 37);
-//		
-//		a.mergePath(bestPath);
-//		
-//		System.out.println();
-//		System.out.println("The best path:");
-//		a.printMap();
-//	}
+
+	
+
+	
+
+	/*public static void main(String[] args) {
+		
+		MazeGenerator mg = new MazeGenerator((40 - 1) / 4, (40 - 1) / 2);
+		
+		AStar a = new AStar(mg.getMaze());
+		
+		System.out.println("The maze to resolve:");
+		a.printMap();
+
+		ArrayList<Node> bestPath = a.findPath(1, 1, 35, 37);
+		
+		a.mergePath(bestPath);
+		
+		System.out.println();
+		System.out.println("The best path:");
+		a.printMap();
+	}*/
 }
