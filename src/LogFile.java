c0298ff1eda77c52dc049e54e9c7e13ef7c725b3
Facundo Acevedo <facevedo@disroot.org>
2017-10-08 18:27:38 -0300

    Multiplataforma - el log reconoce el sistema operativo y crea el archivo dependiendo del mismo

--- src/LogFile.java
+++ src/LogFile.java
@@ -9,7 +9,16 @@ public class LogFile {
 
 
 	public static void log(String st){
-		try (PrintWriter p = new PrintWriter(new FileOutputStream("C:\\Users\\Edu\\Documents\\log\\test.txt", true))) {
+		//multiplataforma
+		String outputLog;
+		String so = System.getProperty("os.name").toLowerCase();
+		if (so.indexOf("nux") >= 0) {
+			 outputLog="/tmp/ia.log";
+		}
+		else {
+			 outputLog="C:\\Users\\Edu\\Documents\\log\\test.txt";
+		}
+		try (PrintWriter p = new PrintWriter(new FileOutputStream(outputLog,  true))) {
 			//AGREGO LA FECHA Y HORA PARA EL LOG
 			Date d = new Date();
 			DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
