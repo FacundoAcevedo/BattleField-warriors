53cd6cd7afa04e2769c3bc4927388e53d2cfb5e7
Eduardo Zanotti <ezanotti@prezano.com>
2017-11-05 22:06:24 -0300

    agregados para atacar, y rajar

--- src/Cthulhu.java
+++ src/Cthulhu.java
@@ -6,6 +6,7 @@ import ia.battle.core.Warrior;
 import ia.battle.core.WarriorData;
 import ia.battle.core.WarriorManager;
 import ia.battle.core.actions.Action;
+import ia.battle.core.actions.Attack;
 import ia.battle.core.actions.Move;
 import ia.battle.core.actions.Skip;
 import ia.exceptions.RuleException;
@@ -29,35 +30,55 @@ public class Cthulhu extends Warrior {
 				// recorre todos los warriors ( el mismo es uno) y evita al
 				// hunter
 
-				if (warrior != this
-						&& warrior.getPosition() != BattleField.getInstance().getHunterData().getFieldCell()) {
-
+				if (warrior != this && warrior.getPosition() != BattleField.getInstance().getHunterData().getFieldCell()) {
 					Warrior targetWarrior = warrior;
 					// lo voy a buscar
 					FieldCell origen = this.getPosition();
 					FieldCell destino = targetWarrior.getPosition();
-
+					ArrayList<FieldCell> caja = BattleField.getInstance().getSpecialItems();
 					AStarSingleton estrellita = AStarSingleton.getInstance();
 					ArrayList<FieldCell> ruta = estrellita.findPath(origen, destino);
-					// ESTA ES LA ACCION PARA REALIZAR EL MOVIMIENTO
-					action = new Move() {
-						@Override
-						public ArrayList<FieldCell> move() {
-							ArrayList<FieldCell> miCamino = new ArrayList<>();
-							for (FieldCell n : ruta) {
-								
-								miCamino.add(n);
+					//si tengo al hunter cerca...
+					if(BattleField.getInstance().getHunterData().getInRange()) {
+						action = new Move() {
+							@Override
+							public ArrayList<FieldCell> move() {
+								ArrayList<FieldCell> miCamino = new ArrayList<>();
+								//y hay cajas en el campo, la voy a buscar
+								if(!caja.isEmpty()) {
+									ArrayList<FieldCell> rutaCaja = estrellita.findPath(origen, caja.get(0));
+									for (FieldCell n : rutaCaja) {
+										miCamino.add(n);
+									}
+									return miCamino;
+								}
+								//sino rajo para otro lado.
+								else {
+									return null;
+								}
 							}
-
-							return miCamino;
-						}
-					};
-
+						};
+					}
+					//si el enemigo esta en rango le da murra
+					else if(BattleField.getInstance().getEnemyData().getInRange()) {
+						return new Attack(BattleField.getInstance().getEnemyData().getFieldCell());
+					}
+					else { //lo va a buscar
+						// ESTA ES LA ACCION PARA REALIZAR EL MOVIMIENTO
+						action = new Move() {
+							@Override
+							public ArrayList<FieldCell> move() {
+								ArrayList<FieldCell> miCamino = new ArrayList<>();
+								for (FieldCell n : ruta) {
+									miCamino.add(n);
+								}
+								return miCamino;
+							}
+						};
+					}
 				}
 			}
-
 			return action;
-
 		} catch (RuleException e) {
 			// TODO Auto-generated catch block
 		}
